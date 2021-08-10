a40d65539a28cd069d2d6bdf1e896c52423e1372
Galtz <galtz0321@gmail.com>
2021-08-10 11:50:30 +0800

    build: exec test when docker build and add revision id

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,17 +2,19 @@
 
 set -ex
 
+REVISION_ID="$(git log -1 --format=%H)"
 SH_DIR="$(cd "$(dirname "$0")"; pwd -P)"
-ROOT_DIR=$(dirname $SH_DIR)
-BUILD_DIR=$ROOT_DIR/build
-DOCKERFILE=$BUILD_DIR/Dockerfile
+ROOT_DIR="$(dirname $SH_DIR)"
+BUILD_DIR="${ROOT_DIR}/build"
+DOCKERFILE="${BUILD_DIR}/Dockerfile"
 NGINX_CONF=./nginx/nginx.conf
 
-source $BUILD_DIR/build-image.properties
+source ${BUILD_DIR}/build-image.properties
 
 docker build \
-  --build-arg NGINX_PORT=$NGINX_PORT \
-  --build-arg NGINX_CONF=$NGINX_CONF \
-  --tag $IMAGE_NAME \
-  --file $DOCKERFILE \
+  --build-arg REVISION_ID=${REVISION_ID} \
+  --build-arg NGINX_PORT=${NGINX_PORT} \
+  --build-arg NGINX_CONF=${NGINX_CONF} \
+  --tag ${IMAGE_NAME} \
+  --file ${DOCKERFILE} \
   $ROOT_DIR
--- scripts/cloud-build.sh
+++ scripts/cloud-build.sh
@@ -2,8 +2,15 @@
 
 set -ex
 
+REVISION_ID="$(git log -1 --format=%H)"
 SH_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 ROOT_DIR=$(dirname $SH_DIR)
+BUILD_DIR="${ROOT_DIR}/build"
+NGINX_CONF=./nginx/nginx.conf
+
+source ${BUILD_DIR}/build-image.properties
 
 cd $ROOT_DIR
-gcloud builds submit --config cloudbuild.yaml
+gcloud builds submit \
+  --config cloudbuild.yaml \
+  --substitutions=REVISION_ID=${REVISION_ID},_NGINX_PORT=${NGINX_PORT},_NGINX_CONF=${NGINX_CONF}
